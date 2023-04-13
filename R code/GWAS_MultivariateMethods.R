fa5f9198221d2d9e9bca9fcf6dd4309b8272ab55
csanchezsarria <csanchezsarria23@gmail.com>
2023-04-13 08:15:30 -0500

    Update format + Add libraries per method

--- R code/GWAS_MultivariateMethods.R
+++ R code/GWAS_MultivariateMethods.R
@@ -1,4 +1,4 @@
-# Short name: Multivariate methods for GWAS covariates
+# Short name: Multivariate methods for GWAS covariable selection
 # Description: It can perform several multivariate methods (MDS, PCA, DAPC) to identify covariates for GWAS 
 # Output(s): It depends on the chosen method. For example:
 #            - NDMS:
@@ -19,56 +19,77 @@
 # 3: For DAPC: Customizable labels
 # 4: Finish the head description
 
-library(SNPRelate)
-library(gdsfmt)
-library(ggplot2)
-library(ggtree)
-library(ape)
-library(plotly)
-library(tidyverse)
-library(psych)
-
-annotation <- read.csv('GWAS_PPD.labels.csv')
-vcf <- 'GWAS_PPD.snps.filter_info.missing_0.10.imputation.vcf.gz'
-gds <- 'GWAS_PPD.gds'
-snpgdsVCF2GDS(vcf, gds, ignore.chr.prefix = "chromosome")
-genofile <- snpgdsOpen(gds)
-sample_id <- read.gdsn(index.gdsn(genofile, "sample.id"))
-
 
 
 # 1: Non-metric multidimensional scaling (NDMS) --------------------------------
 
+# traits: A matrix/database of genotypes/individuals in rows and their traits in in columns
+# names: 
+# extra: 
+# dist: Dissimilarity index/measure to use. The default is euclidean.
+
+setwd("D:/OneDrive - CGIAR/Cassava_Bioinformatics_Team/02_CTS_Drought_Family/01_Phenotype_Preliminar_Analysis/")
+traits <- read.csv("results_treatment_1.csv", header = T) %>%
+  rename(PAR = Light.Intensity..PAR., PS1AC = PS1.Active.Centers, PS1OC = PS1.Oxidized.Centers, gH = gH.) %>%
+  select(Genotipo, LEF, PAR, NPQt, Phi2, PS1AC, PS1OC, PhiNO, SPAD, gH, kP700, PhiNPQ)
+  
+traits <- traits %>% replace(. == "null", NA) %>% replace(. == "NULL", NA)
+traits <- na.omit(traits)
+traits <- traits[!duplicated(traits$Genotipo), ]
+traits$PS1OC <- as.numeric(traits$PS1OC)
+traits$gH <- as.numeric(traits$gH)
+traits <- abs(traits)
+
+
+
+
 NDMS <- function(traits, distance){}
 
-d.m <- metaMDS(traits1, distance = "euclidian", binary = F, autotransform = T)
+# Load libraries
+library(vegan)
+library(tidyverse)
+library(ggrepel)
+library(plotly)
+library(htmlwidgets)
 
-score <- as.data.frame(scores(d.m, display = c("sites", "species")))
-score <- tibble::rownames_to_column(score, "VALUE")
-score <- select(score, NMDS1, NMDS2, VALUE) %>%
-  tidyr::separate(col = VALUE, into = c("Genotipo", "trata"), sep = paste("---"))
+# Define principal arguments
+names <- traits[1]
+traits <- traits[-1]
+dist <- c("bray")
 
-# Plot
-ggplot(score, aes(NMDS1, NMDS2, color = trata)) +
+# Perform MDS
+MDS <- metaMDS(traits, distance = dist, binary = F, autotransform = T)
+MDS.pd <- as.data.frame(scores(MDS, display = c("sites")))
+
+# Merge MDS data with their names
+MDS.f <- cbind(names, MDS.pd)
+
+# Plot MDS
+ggplot(MDS.f, aes(NMDS1, NMDS2)) +
   geom_point() +
-  geom_text_repel(data = score_f, aes(x = NMDS1, y = NMDS2, label = Genotipo), color = "black",
-                  size = 3.5, box.padding = unit(0.9, "lines"), point.padding = unit(0.9, "lines")) +
-  labs(color = "Treatment") +
+  labs() +
   theme_classic() +
   theme(axis.text = element_text(size = 14, angle = 0, color = "black"),
         axis.title = element_text(size = 14, angle = 0, color = "black"),
         axis.title.y = element_text(margin = margin(r = 10)),
-        axis.title.x = element_text(margin = margin(t = 10)),
-        legend.text = element_text(size = 14, color = "black"),
-        legend.position = "bottom", legend.title = element_text(size = 14, angle = 0, color = "black"))
+        axis.title.x = element_text(margin = margin(t = 10)))
 
 
 
 # 2: Multidimensional scaling (MDS) --------------------------------------------
 
+traits <- xxxx
+met <- c("euclidian")
+
 MDS <- function(traits){}
 
-d.m1 <- vegdist(traits1[,2:10], method = "euclidian") # dist function can also be used
+library(vegan)
+library(tidyverse)
+library(ggrepel)
+library(plotly)
+library(htmlwidgets)
+
+d.m1 <- vegdist(traits[,2:10], method = met) # dist function can also be used
 mds.stuff <- cmdscale(d.m1, eig = T, x.ret = T)
 mds.var.per <- round(mds.stuff$eig / (sum(mds.stuff$eig) * 100), 3)
 mds.values <- mds.stuff$points
@@ -97,7 +118,21 @@ ggplot(mds.data, aes(x, y, color = trata)) +
 
 # 3: Principal component analysis (PCA) ----------------------------------------
 
+annotation <- read.csv('GWAS_PPD.labels.csv')
+vcf <- 'GWAS_PPD.snps.filter_info.missing_0.10.imputation.vcf.gz'
+gds <- 'GWAS_PPD.gds'
+snpgdsVCF2GDS(vcf, gds, ignore.chr.prefix = "chromosome")
+genofile <- snpgdsOpen(gds)
+sample_id <- read.gdsn(index.gdsn(genofile, "sample.id"))
+
 PCA <- function(genofile){}
+
+library(SNPRelate)
+library(gdsfmt)
+library(tidyverse)
+library(psych)
+library(plotly)
+library(htmlwidgets)
 
 pca <- snpgdsPCA(genofile, autosome.only = T, remove.monosnp = T, algorithm = "exact",
                  eigen.method = "DSPEVX", need.genmat = T)
@@ -199,25 +234,25 @@ fig
 
 # Save the plot
 saveWidget(fig, "GWAS_PCA.html", selfcontained = F, libdir = "lib")
-htmlwidgets::saveWidget(as_widget(fig), "GWAS_PCA.html")
-
-
+saveWidget(as_widget(fig), "GWAS_PCA.html")
 
 
 
 # 4: Discriminant analysis of principal components (DAPC) ----------------------
 
-DAPC <- function(vcf){}
-
-# Load packages and data
-library(adegenet)
-library(vcfR)
-library(tibble)
-
 vcf <- read.vcfR('GWAS_PPD.snps.filter_info.missing_0.10.imputation.vcf.gz', verbose = T)
 my_genind <- vcfR2genind(vcf)
 my_genind
 
+DAPC <- function(vcf){}
+
+# Load packages and data
+library(adegenet)
+library(grDevices)
+library(vcfR)
+library(tidyverse)
+library(plotly)
+library(htmlwidgets)
 
 # Identify clusters
 # Shows a graph with the accumulated variance explained by the eigenvalues of the PCA
