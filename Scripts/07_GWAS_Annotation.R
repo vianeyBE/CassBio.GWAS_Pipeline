9547a30f4ddf69938d20b0ebe630680197fa62e4
Camilo E. Sánchez <csanchezsarria23@gmail.com>
2025-08-15 11:17:45 -0500

    Full changes to the function

--- Scripts/07_GWAS_Annotation.R
+++ Scripts/07_GWAS_Annotation.R
@@ -1,28 +1,30 @@
 # Short name: Annotation for GWAS results
-# Description (1): Get the annotation of the gen containing the SNP using the GWAS GAPIT results
-# Description (2): Retrieves the closest genes down and up stream
+# Description (1): Get the annotation of the SNP using the GWAS GAPIT results
+# Description (2): Retrieves the closest bp down and up stream
 # Output: Basename of the output file
 # 
-# Authors: Camilo E. Sanchez (c.e.sanchez@cgiar.org) and Vianey Barrera-Enriquez (vpbarrera@gmail.com)
+# Authors: Camilo E. Sanchez (c.e.sanchez@cgiar.org)
+# Authors: Vianey Barrera-Enriquez (vpbarrera@gmail.com)
 #
 # Arguments:
-# version: You can choose among the different reference genome versions (Options: 6.1 or 8.1).
-# Wdir: Directory that contains the GAPIT results (Example: home/user/folder).
-# name: The path of file names to look for. (Example: QTL_LOD_Intervals).
-# mod: The model(s) of interest (Options: BLINK, GLM, MLM, FarmCPU, etc).
-# wdyw: What are you looking for to annotate (Options: CDS, five_prime_UTR, gene, mRNA, three_prime_UTR).
+# version: Choose among the reference genome versions (Options: 6.1 or 8.1)
+# recursive: 
+# pb: 
+# dir: Directory that contains the GAPIT results (Example: home/user/folder)
+# file_name: The path of file names to look for. (Example: QTL_LOD_Intervals)
+# mod: The model(s) of interest (Options: BLINK, GLM, MLM, FarmCPU, etc)
+# wdyw: What to annotate (Options: CDS, five_prime_UTR, gene, mRNA, three_prime_UTR)
 
 
 
 ###### To do ######
-# 1: Add PVE part to the recursive
-# 2: Delete duplicates
+# Everything is ok!
 
 
 
 # 0: Function init -------------------------------------------------------------
 
-GWAS_Annotation <- function(version, recursive, Wdir, name, mod, wdyw){
+GWAS_Annotation <- function(version, recursive, pb, dir, file_name, mod, wdyw){
   
   
   
@@ -80,8 +82,8 @@ GWAS_Annotation <- function(version, recursive, Wdir, name, mod, wdyw){
     
     # Read and modify the files
     annot <- read.delim(paste0(Ddir, annot), header = T) %>%
-      rename(ID = 1, Name = 2, Trans = 3, Peptide = 4, GO = 10, AT.name = 11, Annotation = 12) %>%
-      select(ID, Name, GO, Annotation)
+      rename(ID = 1, Name = 2, GO = 10, Annotation = 12) %>%
+      select(Name, GO, Annotation)
     
     gff <- read.delim(paste0(Ddir, gff), header = F, comment.char = "#") %>%
       rename(Chr = V1, What = V3, Start = V4, End = V5, Strand = V7) %>%
@@ -107,14 +109,15 @@ GWAS_Annotation <- function(version, recursive, Wdir, name, mod, wdyw){
   # 2: Read GWAS results and filter them ---------------------------------------
   
   # Set working directory
-  setwd(Wdir)
+  setwd(dir)
   
   # Read GWAS results files
   if (recursive == T){
     
     # Get the names of the files
     message("Getting list of CSV files...")
-    names <- list.files(path = Wdir, pattern = paste0(name, "."), all.files = F, full.names = F, recursive = T)
+    names <- list.files(path = dir, pattern = paste0(name, "."), 
+                        all.files = F, full.names = F, recursive = T)
     
     # Informative messages
     message(paste("GWAS files found:", length(names)))
@@ -133,17 +136,19 @@ GWAS_Annotation <- function(version, recursive, Wdir, name, mod, wdyw){
       mdl <- str_split_1(int[2], paste0(".", tr[1]))
       
       # Database handling
-      csv_L[[i]] <- read.csv(paste0(Wdir, names[i])) %>%
-        mutate(Trait = tr[1], Model = mdl[1], Start = Pos - 10000, End = Pos + 10000) %>%
-        select(SNP, Chr, Pos, Start, End, P.value, MAF, Effect, Model, Trait) %>%
+      csv_L[[i]] <- read.csv(paste0(dir, names[i])) %>%
+        mutate(Trait = tr[1], Model = mdl[1], Start = Pos - pb, End = Pos + pb) %>%
+        select(SNP, Chr, Pos, Start, End, P.value, MAF, Effect, PVE, 
+               Model, Trait) %>%
         filter(P.value <= (0.05/length(SNP)))
       
       # Progress bar
-      cat('\r', i, ' files processed |', rep('=', i / 2), ifelse(i == length(names), '|\n', '>'), sep = '')
+      cat('\r', i, ' files processed |', rep('=', i / 2), 
+          ifelse(i == length(names), '|\n', '>'), sep = '')
       
     }
     
-    # Merge the data frames of the list in a single data frame and filter by model type
+    # Merge dataframes of the list in a single data frame and filter by model type
     GWAS <- bind_rows(csv_L)
     GWAS <- filter(GWAS, Model %in% mod)
     
@@ -151,12 +156,9 @@ GWAS_Annotation <- function(version, recursive, Wdir, name, mod, wdyw){
     
   } else {
     
-    GWAS <- read.csv(name) %>%
-      mutate(Start = Pos - 10000, End = Pos + 10000) %>%
+    GWAS <- read.csv(paste0(dir, file_name)) %>%
       rename(PValue = P.value) %>%
-      select(Trait, Model, SNP, Chr, Pos, Start, End, PValue, MAF, Effect, PVE) %>%
-      filter(PValue <= (0.05/length(SNP))) %>%
-      filter(Model %in% mod)
+      select(Trait, Model, SNP, Chr, Pos, PValue, MAF, Effect, PVE)
     
   }
   
@@ -168,7 +170,7 @@ GWAS_Annotation <- function(version, recursive, Wdir, name, mod, wdyw){
     
     
     # 3: Match the results with the gene annotation database -------------------
-    
+    # Informative message
     message("Retriving annotation...")
     
     # Creates an empty list to store the data
@@ -177,45 +179,31 @@ GWAS_Annotation <- function(version, recursive, Wdir, name, mod, wdyw){
     # Filters and merges the gff3 data
     for (i in 1:nrow(GWAS)){
       
-      # Filter the gff3 file per chromosome
-      data <- dplyr::filter(gff, GWAS$Chr[i] == Chr)
-      data.der <- dplyr::filter(data, GWAS$Pos[i] <= Start)
-      data.izq <- dplyr::filter(data, GWAS$Pos[i] >= End)
+      # SNP info
+      snp_chr <- GWAS$Chr[i]
+      snp_pos <- GWAS$Pos[i]
       
-      # Combine the different filtered data
-      gff_L[[i]] <- rbind(
-        
-        # Genes in a 10.000 window 
-        dplyr::filter(data, GWAS$Pos[i] >= Start & GWAS$Pos[i] <= End) %>%
-          mutate(SNP = GWAS$SNP[i], SNP.Pos = GWAS$Pos[i], 
-                 SNP.Start = GWAS$Start[i], SNP.End = GWAS$End[i], 
-                 SNP.Location = "Inside", Distance = "---",
-                 Pvalue = GWAS$PValue[i], MAF = GWAS$MAF[i], Effect = GWAS$Effect[i],
-                 Model = GWAS$Model[i], Trait = GWAS$Trait[i], PVE = GWAS$PVE[i]),
-        
-        # Looking genes down stream 
-        data.der[which.min(abs(GWAS$Pos[i] - data.der$Start)),] %>%
-          mutate(SNP = GWAS$SNP[i], SNP.Pos = GWAS$Pos[i], 
-                 SNP.Start = GWAS$Start[i], SNP.End = GWAS$End[i], 
-                 SNP.Location = "Right", Distance = min(abs(GWAS$Pos[i] - data.der$Start)),
-                 Pvalue = GWAS$PValue[i], MAF = GWAS$MAF[i], Effect = GWAS$Effect[i],
-                 Model = GWAS$Model[i], Trait = GWAS$Trait[i], PVE = GWAS$PVE[i]),
-        
-        # Looking genes up stream
-        data.izq[which.min(abs(GWAS$Pos[i] - data.izq$End)),] %>%
-          mutate(SNP = GWAS$SNP[i], SNP.Pos = GWAS$Pos[i],
-                 SNP.Start = GWAS$Start[i], SNP.End = GWAS$End[i],
-                 SNP.Location = "Left", Distance = min(abs(GWAS$Pos[i] - data.izq$End)),
-                 Pvalue = GWAS$PValue[i], MAF = GWAS$MAF[i], Effect = GWAS$Effect[i],
-                 Model = GWAS$Model[i], Trait = GWAS$Trait[i], PVE = GWAS$PVE[i])
-        
-      )
-      
-      # Convert the values of the column into characters to avoid issues
-      gff_L[[i]]$Distance <- as.character(gff_L[[i]]$Distance)
+      # Filter all the data by chr and position less than pb
+      gff_L[[i]] <- dplyr::filter(gff, Chr == snp_chr) %>%
+        mutate(Dist_SNP_to_Start = abs(Start - snp_pos),
+               Dist_SNP_to_End = abs(End - snp_pos)) %>%
+        filter(Dist_SNP_to_Start <= pb | Dist_SNP_to_End <= pb) %>%
+        filter(!duplicated(Name)) %>%
+        mutate(Trait = GWAS$Trait[i], Model = GWAS$Model[i], SNP = GWAS$SNP[i],
+               SNP_Chr = GWAS$Chr[i], SNP_Pos = GWAS$Pos[i],  
+               Pvalue = GWAS$PValue[i], MAF = GWAS$MAF[i],
+               Effect = GWAS$Effect[i], PVE = GWAS$PVE[i]) %>%
+        select(Trait, Model, SNP, SNP_Chr, SNP_Pos, Pvalue, MAF, Effect, PVE, 
+               Name, Start, End, Strand, Dist_SNP_to_Start, Dist_SNP_to_End) %>%
+        mutate(Location = case_when(
+          SNP_Pos > Start & SNP_Pos < End ~ "Inside",
+          SNP_Pos < Start & SNP_Pos < End ~ "Upstream",
+          SNP_Pos > Start & SNP_Pos > End ~ "Downstream",
+          TRUE ~ NA_character_))
       
       # Progress bar
-      cat('\r', i, ' files processed |', rep('=', i / 3), ifelse(i == nrow(GWAS), '|\n', '>'), sep = '')
+      cat('\r', i, ' SNPs processed |', rep('=', i / 3), 
+          ifelse(i == nrow(GWAS), '|\n', '>'), sep = '')
       
     }
     
@@ -227,44 +215,34 @@ GWAS_Annotation <- function(version, recursive, Wdir, name, mod, wdyw){
     
     
     # 4: Formatting dataframe --------------------------------------------------
-    
+    # Informative message
     message("Obtaining gene information...")
     
-    # Creates an empty list to store the data
-    annot_L <- list()
+    # Left join between genetic data and annotation 
+    SNP_annotation <- left_join(gff_LM, annot, by = "Name",
+                                relationship = "many-to-many") %>%
+      mutate(ID = paste0(Trait, Model, SNP, PVE, Name, Annotation)) %>%
+      #filter(Annotation != "") %>%
+      filter(!duplicated(ID)) %>%
+      select(-ID)
     
-    # Filters and merges the annot data
-    for (i in 1:nrow(gff_LM)){
-      
-      # Filter the annotation data and creates new columns based on previous data
-      annot_L[[i]] <- filter(annot, Name == gff_LM$Name[i]) %>%
-        mutate(Chr = gff_LM$Chr[i], Effect = gff_LM$Effect[i], 
-               Pvalue = gff_LM$Pvalue[i], MAF = gff_LM$MAF[i], 
-               PVE = gff_LM$PVE[i], Model = gff_LM$Model[i], 
-               Trait = gff_LM$Trait[i], Distance = gff_LM$Distance[i],
-               Gen.Start = gff_LM$Start[i], Gen.End = gff_LM$End[i], 
-               SNP.Location = gff_LM$SNP.Location[i], SNP = gff_LM$SNP[i], 
-               Pos = gff_LM$SNP.Pos[i], Strand = gff_LM$SNP[i])
-      
-      # Progress bar
-      cat('\r', i, ' files processed |', rep('=', i / 4), 
-          ifelse(i == nrow(gff_LM), '|\n', '>'), sep = '')
-      
-    }
-    
-    # Merge the data frames of the list in a single data frame and modify it
-    SNP_annotation <- bind_rows(annot_L) %>%
-      select(Trait, Model, SNP, Chr, Pos, Pvalue, Effect, MAF, PVE, Name, 
-             Gen.Start, Gen.End, GO, Annotation, SNP.Location, Distance)
+    # Informative message
+    message(paste("There are", dim(SNP_annotation)[1], "genes annotated"))
     
     
     
     # 5: Save output -----------------------------------------------------------
+    # Informative message
+    message("Saving output file: ", 
+            paste0(gsub(".csv", "", file_name)),
+            "_GWAS_Annotation.csv in working directory")
     
-    message("Saving output file: 'GWAS_Annotation.csv' in working directory")
+    # Actually saves it!
+    write.csv(SNP_annotation, 
+              file = paste0(gsub(".csv", "", file_name), "_GWAS_Annotation.csv"), 
+              row.names = F)
     
-    write.csv(SNP_annotation, file = "GWAS_Annotation.csv", quote = F, row.names = F)
-    
+    # Informative
     message("Done!")
     
   } else {
@@ -279,14 +257,15 @@ GWAS_Annotation <- function(version, recursive, Wdir, name, mod, wdyw){
 
 # Example(s) -------------------------------------------------------------------
 # Set arguments
-# version <- "8.1"
-# recursive <- "F"
-# Wdir <- "D:/OneDrive - CGIAR/00_BioInf_Platform/09_DiversityPanel/03_GWAS_Camilo/"
-# name <- "Summary_GWAS_Results.csv"
-# mod <- c("BLINK", "FarmCPU", "MLMM")
-# wdyw <- "gene"
+ version <- "8.1"
+ recursive <- "F"
+ pb <- 10000
+ dir <- "D:/OneDrive - CGIAR/00_BioInf_Platform/04_CBSD_Group6/10_Annotation/"
+ file_name <- "GWAS.csv"
+ mod <- c("GLM", "MLMM", "BLINK")
+ wdyw <- "gene"
 
 
 
 # Run function -----------------------------------------------------------------
-# GWAS_Annotation(version, recursive, Wdir, name, mod, wdyw)
+# GWAS_Annotation(version, recursive, pb, dir, file_name, mod, wdyw)
